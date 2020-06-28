04a5f535d9d9772adaeed1a7b93198c55bd52936
valeryshkuratov <valery.shkuratov.01@mail.ru>
2020-06-28 21:16:06 +0300

    git add -A

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static int NOD(int m, int n)
+        {
+            while (m != n)
+            {
+                if (m > n)
+                {
+                    m = m - n;
+                }
+                else
+                {
+                    n = n - m;
+                }
+            }
+            return m;
+
+        }
+        
+        public static int NOK(int m, int n)
+        {
+            int i;
+            i = (m * n) / NOD(m, n);
+            return i;
+        }
+
+        public static string simpleNum(int m)
+        {
+            int k = 0;
+            for (int i=1; i<m; i++)
+            {
+                if (m % i == 0)
+                    k++;
+            }
+            if (k > 1 || m==1)
+                return "Число составное";
+            else
+                return "Число простое";
+        }
+    }
+}
